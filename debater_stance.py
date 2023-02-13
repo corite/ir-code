43e3be3894d6b7256f594735b4dd63ee2365d5f0
Neville Longbottom <neville@yuustan.space>
2023-02-13 15:03:38 +0100

    Using content from index, added stance column

--- debater_stance.py
+++ debater_stance.py
@@ -1,44 +1,45 @@
 from debater_python_api.api.debater_api import DebaterApi
-from neville.stance_detector import StanceDetector
+from neville.stance import StanceDetector
+import nltk
 import pandas as pd
 import shelve
 
 class DebaterStanceDetector(StanceDetector):
     
-    def __init__(self, stance, images, content_extractor, api_token, cache_file='/tmp/debater-cache.db'):
-        super().__init__(stance, images, content_extractor)
+    def __init__(self, index, images, api_token, sentences=None, cache_file='/tmp/debater-cache'):
+        super().__init__(index, images)
         self.api = DebaterApi(api_token)
+        self.sentences = sentences
         self.cache_file = cache_file
 
-    def get_stances(self, query_docno_sentences: [(str, str, str)]):
+    def get_stances(self, stance_query_docno_sentences: [(str, str, str, str)]):
         '''computes the stance of each sentence in the range of [-1, 1]'''
-        if len(query_docno_sentences) == 0:
+        if len(stance_query_docno_sentences) == 0:
             return []
         pro_con_client = self.api.get_pro_con_client()
         pro_con_client.set_show_process(False)
-        sentence_topic_dicts = [{'sentence': sentence, 'topic': query } for query, docno, sentence in query_docno_sentences]
+        sentence_topic_dicts = [{'sentence': sentence, 'topic': query } for stance, query, docno, sentence in stance_query_docno_sentences]
         return pro_con_client.run(sentence_topic_dicts)
     
-    def score(self, query_docno_sentences: ((str, str, str))) -> [float]:
+    def score(self, stance_query_docno_text: ((str, str, str, str))) -> pd.DataFrame:
+        stance_query_docno_sentences = ((stance, query, docno, sentence) for stance, query, docno, text in stance_query_docno_text for sentence in nltk.sent_tokenize(text)[:self.sentences])
+        
         with shelve.open(self.cache_file) as db:
-            query_docno_values = []
-            non_cached_query_docno_sentences = []
-            for query, docno, sentence in query_docno_sentences:
+            stance_query_docno_score = []
+            non_cached_stance_query_docno_sentences = []
+            for stance, query, docno, sentence in stance_query_docno_sentences:
                 key = str((query, sentence))
                 if key in db:
-                    query_docno_values.append((query, docno, db[key]))
+                    stance_query_docno_score.append((stance, query, docno, db[key]))
                 else:
-                    non_cached_query_docno_sentences.append((query, docno, sentence))
+                    non_cached_stance_query_docno_sentences.append((stance, query, docno, sentence))
                     
-            stances = self.get_stances(non_cached_query_docno_sentences)
+            stances = self.get_stances(non_cached_stance_query_docno_sentences)
             
-            for value, (query, docno, sentence) in zip(stances, non_cached_query_docno_sentences):
-                db[str((query, sentence))] = value
-                query_docno_values.append((query, docno, value))
+            for score, (stance, query, docno, sentence) in zip(stances, non_cached_stance_query_docno_sentences):
+                db[str((query, sentence))] = score
+                stance_query_docno_score.append((stance, query, docno, score))
 
-            df = pd.DataFrame(query_docno_values, columns=['query', 'docno', 'score'])
-            df_grouped = df.groupby(['query', 'docno']).mean()
-            if self.stance == 'CON':
-                df_grouped['score'] *= -1
-            df_grouped['score'] += 1
-            return df_grouped
+            df = pd.DataFrame(stance_query_docno_score, columns=['stance', 'query', 'docno', 'score'])
+            df['score'] = df['score'].where(df['stance'] == 'PRO', -df['score']) + 1
+            return df.groupby(['stance', 'query', 'docno']).mean()
---
+++ stance.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pyterrier as pt
+
+class StanceDetector():
+    
+    def __init__(self, index, images):
+        self.index = index
+        self.images = images
+        
+    def score(self, stance_query_docno_text: ((str, str, str, str))) -> pd.DataFrame:
+        pass
+    
+    def __call__(self, doc_rows):
+        stance_query_docno_text = ((stance, query, docno, self.index.getMetaIndex().getItem('text', self.index.getMetaIndex().getDocument('docno', docno))) for stance, query, docno in zip(doc_rows['stance'], doc_rows['query'], doc_rows['docno']))
+        # important: make sure index of doc_rows is preserved, otherwise returned results can't be matched
+        result = doc_rows.drop(columns=['score']).join(self.score(stance_query_docno_text), on=['stance', 'query', 'docno'])
+        # set documents with no content, therefore not in the left join (NaN), to score 0
+        return result['score'].fillna(0)
+    
+def apply_stance(stance):
+    if stance not in ['PRO', 'CON']:
+        raise ValueError('Stance can only be PRO or CON')
+    return pt.apply.generic(lambda df: df.assign(stance=stance))

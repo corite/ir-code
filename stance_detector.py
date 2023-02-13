136cf7d7caa76047165cae442d1bdf1b4ff161f6
Neville Longbottom <neville@yuustan.space>
2023-02-13 13:30:39 +0100

    use __call__ instead of returning partial function

--- stance_detector.py
+++ stance_detector.py
@@ -1,4 +1,3 @@
-from functools import partial
 import pandas as pd
 
 class StanceDetector():
@@ -13,12 +12,9 @@ class StanceDetector():
     def score(self, query_docno_sentences: ((str, str, str))) -> float:
         return 0
     
-    def _rerank(self, doc_rows):
+    def __call__(self, doc_rows):
         query_sentences = ((query, image_id, sentence) for query, image_id in zip(doc_rows['query'], doc_rows['docno']) for sentence in self.content_extractor(self.images[image_id]))
         # important: make sure index of doc_rows is preserved, otherwise returned results can't be matched
         result = doc_rows.drop(columns=['score']).join(self.score(query_sentences), on=['query', 'docno'])
         # set documents with no content, therefore not in the left join (NaN), to score 0
         return result['score'].fillna(0)
-    
-    def rerank(self):
-        return partial(StanceDetector._rerank, self)
